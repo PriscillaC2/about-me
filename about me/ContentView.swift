1d75ea8aee94329cf5e2755a5a12972eb5c61a7a
Priscilla Contreras <Priscilla.contreras.08@gmail.com>
2024-07-25 14:38:46 -0700

    Initial Commit

---
+++ about me/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  about me
+//
+//  Created by Scholar on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemGray5)
+                .ignoresSafeArea()
+        VStack (alignment: .leading,
+                spacing: 20.0){
+            Text("Priscilla Contreras")
+                .font(.title)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.orange)
+                .background(Rectangle())
+                .foregroundColor(Color.white)
+                .cornerRadius(15)
+            Text("My Interests")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.orange)
+                .multilineTextAlignment(.center)
+                . background(Rectangle())
+                .foregroundColor(Color.white)
+                .cornerRadius(15)
+            Text("Soccer")
+                .foregroundColor(Color.orange)
+            Image("Soccer")
+                .renderingMode(.original)
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                
+            Text("Photography")
+                .foregroundColor(Color.orange)
+            Image("camera")
+                .renderingMode(.original)
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            Text("Baking")
+
+                .foregroundColor(Color.orange)
+            Image("cookie")
+                .renderingMode(.original)
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                
+            }
+        }
+    }
+}
+        
+#Preview {
+    ContentView()
+}
